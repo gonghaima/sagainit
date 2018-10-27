da330eeb833f1f2711bc1d3cd57b6d10cd7d1550
Steven Gong <steven.gong@iag.com.au>
2018-10-28 09:33:47 +1100

    initial saga setup

--- main.js
+++ main.js
@@ -3,11 +3,16 @@ import "babel-polyfill"
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 
 import Counter from './Counter'
 import reducer from './reducers'
+import { helloSaga } from './sagas'
 
-const store = createStore(reducer)
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(reducer, applyMiddleware(sagaMiddleware))
+
+sagaMiddleware.run(helloSaga)
 
 const action = type => store.dispatch({type})
 
