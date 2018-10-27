a933d3b6c469bdba86f286500c527f640f0a3ae4
Steven Gong <steven.gong@iag.com.au>
2018-10-28 10:19:43 +1100

    Making Asynchronous calls

--- Counter.js
+++ Counter.js
@@ -1,8 +1,12 @@
 /*eslint-disable no-unused-vars */
 import React, { Component, PropTypes } from 'react'
 
-const Counter = ({ value, onIncrement, onDecrement }) =>
+const Counter = ({ value, onIncrement, onDecrement, onIncrementAsync }) =>
       <div>
+        <button onClick={onIncrementAsync}>
+          Increment after 1 second
+        </button>
+        {' '}
         <button onClick={onIncrement}>
           Increment
         </button>
--- main.js
+++ main.js
@@ -1,30 +1,32 @@
-import "babel-polyfill"
+import "babel-polyfill";
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
-import createSagaMiddleware from 'redux-saga'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
 
-import Counter from './Counter'
-import reducer from './reducers'
-import { helloSaga } from './sagas'
+import Counter from "./Counter";
+import reducer from "./reducers";
+import rootSaga from "./sagas";
 
-const sagaMiddleware = createSagaMiddleware()
-const store = createStore(reducer, applyMiddleware(sagaMiddleware))
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
 
-sagaMiddleware.run(helloSaga)
+sagaMiddleware.run(rootSaga);
 
-const action = type => store.dispatch({type})
+const action = type => store.dispatch({ type });
 
 function render() {
   ReactDOM.render(
     <Counter
       value={store.getState()}
-      onIncrement={() => action('INCREMENT')}
-      onDecrement={() => action('DECREMENT')} />,
-    document.getElementById('root')
-  )
+      onIncrement={() => action("INCREMENT")}
+      onDecrement={() => action("DECREMENT")}
+      onIncrementAsync={() => action("INCREMENT_ASYNC")}
+    />,
+    document.getElementById("root")
+  );
 }
 
-render()
-store.subscribe(render)
+render();
+store.subscribe(render);
