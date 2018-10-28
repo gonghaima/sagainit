48d192844555b8117a102790daa9f09aa1a488fb
Steven Gong <steven.gong@iag.com.au>
2018-10-28 14:30:47 +1100

    Making our code testable

--- sagas.js
+++ sagas.js
@@ -1,21 +1,22 @@
-import { delay } from "redux-saga";
-import { put, takeEvery, all } from "redux-saga/effects";
+import { delay } from 'redux-saga';
+import { put, takeEvery, all, call } from 'redux-saga/effects';
 
 export function* helloSaga() {
-  console.log("Hello Sagas!");
+  console.log('Hello Sagas!');
 }
 
-function* incrementAsync() {
-  yield delay(1000);
-  yield put({ type: "INCREMENT" });
+export function* incrementAsync() {
+  // use the call Effect
+  yield call(delay, 1000);
+  yield put({ type: 'INCREMENT' });
 }
 
-function* watchIncrementAsync() {
-  yield takeEvery("INCREMENT_ASYNC", incrementAsync);
+export function* watchIncrementAsync() {
+  yield takeEvery('INCREMENT_ASYNC', incrementAsync);
 }
 
 // notice how we now only export the rootSaga
 // single entry point to start all Sagas at once
 export default function* rootSaga() {
-  yield all([helloSaga(), watchIncrementAsync()]);
+  yield all([helloSaga(), watchIncrementAsync(), incrementAsync()]);
 }
