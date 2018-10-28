0252f3cccbf007ec6393c6bd60509e9f17077ea7
Steven Gong <steven.gong@iag.com.au>
2018-10-28 14:50:10 +1100

    more tests

--- sagas.spec.js
+++ sagas.spec.js
@@ -15,17 +15,17 @@ test('incrementAsync Saga test', assert => {
     'incrementAsync Saga must call delay(1000)'
   );
 
-  //   assert.deepEqual(
-  //     gen.next().value,
-  //     put({ type: 'INCREMENT' }),
-  //     'incrementAsync Saga must dispatch an INCREMENT action'
-  //   );
+  assert.deepEqual(
+    gen.next().value,
+    put({ type: 'INCREMENT' }),
+    'incrementAsync Saga must dispatch an INCREMENT action'
+  );
 
-  //   assert.deepEqual(
-  //     gen.next(),
-  //     { done: true, value: undefined },
-  //     'incrementAsync Saga must be done'
-  //   );
+  assert.deepEqual(
+    gen.next(),
+    { done: true, value: undefined },
+    'incrementAsync Saga must be done'
+  );
 
   assert.end();
 });
